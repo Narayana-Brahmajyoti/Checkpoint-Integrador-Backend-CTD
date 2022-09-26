54586bac247046e2f9eeecc357c0cbaaf39f7e9d
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-09-26 11:08:20 -0300

    Implementando a authentication no user controler

--- src/main/java/com/integrador/odonto/backendquintobimestre/controller/UserController.java
+++ src/main/java/com/integrador/odonto/backendquintobimestre/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.integrador.odonto.backendquintobimestre.controller;
 
 import com.integrador.odonto.backendquintobimestre.entity.dto.UserDTO;
+import com.integrador.odonto.backendquintobimestre.security.AuthenticationResponse;
+import com.integrador.odonto.backendquintobimestre.security.JwtUtil;
 import com.integrador.odonto.backendquintobimestre.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +23,12 @@ public class UserController {
     @Autowired
     private UserServiceImpl userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping
     public ResponseEntity<String> create(@RequestBody UserDTO userDTO) {
         Boolean create = userService.create(userDTO);
@@ -25,5 +37,18 @@ public class UserController {
             return new ResponseEntity<>("Usuário criado com sucesso!", HttpStatus.CREATED);
 
         return new ResponseEntity<>("Usuário não pode ser criado!", HttpStatus.CONFLICT);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> createAuthenticationToken(@RequestBody UserDTO userDTO) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userDTO.getUsername(), userDTO.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect", e);
+        }
+        final UserDetails userDetails = userService.loadUserByUsername(userDTO.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }

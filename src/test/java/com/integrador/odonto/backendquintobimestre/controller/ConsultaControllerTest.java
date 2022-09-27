59a9297b821a2991e448bae43b9f4050e5e994ba
Narayana-Brahmajyoti <nbrahmajyoti@gmail.com>
2022-09-27 18:31:03 -0300

    Teste integrado método create ConsultaControllerTest

--- src/test/java/com/integrador/odonto/backendquintobimestre/controller/ConsultaControllerTest.java
+++ src/test/java/com/integrador/odonto/backendquintobimestre/controller/ConsultaControllerTest.java
@@ -1,11 +1,9 @@
 package com.integrador.odonto.backendquintobimestre.controller;
 
+import com.integrador.odonto.backendquintobimestre.entity.dto.ConsultaDTO;
 import com.integrador.odonto.backendquintobimestre.entity.dto.DentistaDTO;
 import com.integrador.odonto.backendquintobimestre.entity.dto.EnderecoDTO;
 import com.integrador.odonto.backendquintobimestre.entity.dto.PacienteDTO;
-import com.integrador.odonto.backendquintobimestre.service.impl.EnderecoServiceImpl;
-import com.integrador.odonto.backendquintobimestre.service.impl.PacienteServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDateTime;
+
 import static com.integrador.odonto.backendquintobimestre.utils.ClinincaOdontologicaUtils.asJsonString;
 import static com.integrador.odonto.backendquintobimestre.utils.ClinincaOdontologicaUtils.objectFromString;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,17 +28,15 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class PacienteControllerTest {
-
+class ConsultaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext context;
 
-
     @BeforeEach
-    void init() {
+    void init(){
         mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
@@ -49,78 +47,11 @@ class PacienteControllerTest {
     void create() throws Exception{
         EnderecoDTO enderecoDTO = new EnderecoDTO();
         int idEndereco;
-        String rua = "Rua Rio Negro";
-        String numero = "1234";
-        String complemento = "apto 1104";
-        String bairro = "Vera Cruz";
-
-        enderecoDTO.setRua(rua);
-        enderecoDTO.setNumero(numero);
-        enderecoDTO.setComplemento(complemento);
-        enderecoDTO.setBairro(bairro);
-
-
-
-        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/endereco/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .content(asJsonString(enderecoDTO)))
-                        .andDo(MockMvcResultHandlers.print())
-                        .andExpect(MockMvcResultMatchers.status().isOk())
-                        .andReturn();
-
-        String responseBody = mvcResult.getResponse().getContentAsString();
-        responseBody = mvcResult.getResponse().getContentAsString();
-
-        enderecoDTO = objectFromString(EnderecoDTO.class, responseBody);
-
-        PacienteDTO pacienteDTO = new PacienteDTO();
-        String nome = "Gabriela";
-        String sobreNome = "Carvalho";
-        String rg = "MG-15678";
-        String dataDeAlta = "25/10/2022";
-
-
-        pacienteDTO.setNome(nome);
-        pacienteDTO.setSobreNome(sobreNome);
-        pacienteDTO.setEndereco(enderecoDTO);
-        pacienteDTO.setRg(rg);
-        pacienteDTO.setDataDeAlta(dataDeAlta);
-
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/paciente/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .content(asJsonString(pacienteDTO)))
-                        .andDo(MockMvcResultHandlers.print())
-                        .andExpect(MockMvcResultMatchers.status().isOk())
-                        .andReturn();
-
-        responseBody = mvcResult.getResponse().getContentAsString();
-        responseBody = mvcResult.getResponse().getContentAsString();
-
-        pacienteDTO = objectFromString(PacienteDTO.class, responseBody);
-
-        assertNotNull(pacienteDTO.getId());
-        assertNotEquals("Gabi", pacienteDTO.getNome());
-        assertEquals("Gabriela", pacienteDTO.getNome());
-        assertEquals("Carvalho", pacienteDTO.getSobreNome());
-        assertEquals(1,pacienteDTO.getId());
-        assertNotNull(enderecoDTO);
-        assertEquals("Rua Rio Negro", enderecoDTO.getRua());
-    }
-
-    @Test
-    @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
-    void getById() throws Exception{
-        EnderecoDTO enderecoDTO = new EnderecoDTO();
-        int idEndereco;
         enderecoDTO.setRua("Rua Rio Negro");
         enderecoDTO.setNumero("1234");
         enderecoDTO.setComplemento("apto 1104");
         enderecoDTO.setBairro("Vera Cruz");
 
-
-
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/endereco/create")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
@@ -135,6 +66,7 @@ class PacienteControllerTest {
         enderecoDTO = objectFromString(EnderecoDTO.class, responseBody);
 
         PacienteDTO pacienteDTO = new PacienteDTO();
+        int idPaciente;
         pacienteDTO.setNome("Gabriela");
         pacienteDTO.setSobreNome("Carvalho");
         pacienteDTO.setEndereco(enderecoDTO);
@@ -154,28 +86,61 @@ class PacienteControllerTest {
 
         pacienteDTO = objectFromString(PacienteDTO.class, responseBody);
 
-        mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/paciente/getById/{id}", pacienteDTO.getId())
-                        .accept(MediaType.APPLICATION_JSON))
+        DentistaDTO dentistaDTO = new DentistaDTO();
+        int idDentista;
+        dentistaDTO.setNome("Helena");
+        dentistaDTO.setSobreNome("Melo de Campos");
+        dentistaDTO.setMatricula("CD-12785");
+
+        mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/dentista/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(dentistaDTO)))
                         .andDo(MockMvcResultHandlers.print())
                         .andExpect(MockMvcResultMatchers.status().isOk())
                         .andReturn();
 
         responseBody = mvcResult.getResponse().getContentAsString();
-        PacienteDTO pacienteDTO2 = objectFromString(PacienteDTO.class, responseBody);
-        pacienteDTO = objectFromString(PacienteDTO.class, responseBody);
+        responseBody = mvcResult.getResponse().getContentAsString();
 
-        assertNotNull(pacienteDTO.getId());
-        assertEquals(pacienteDTO.getId(), pacienteDTO2.getId());
-        assertNotEquals(1,pacienteDTO.getId());
-        assertNotEquals("Gabi", pacienteDTO.getNome());
-        assertEquals("Gabriela", pacienteDTO.getNome());
-        assertEquals("Carvalho", pacienteDTO.getSobreNome());
-        assertNotNull(enderecoDTO);
-        assertEquals("Rua Rio Negro", enderecoDTO.getRua());
+        dentistaDTO = objectFromString(DentistaDTO.class, responseBody);
+
+        ConsultaDTO consultaDTO = new ConsultaDTO();
+//        int IdConsulta;
+        consultaDTO.setPaciente(pacienteDTO);
+        consultaDTO.setDentista(dentistaDTO);
+        consultaDTO.setDataHoraConsulta(LocalDateTime.of(2022,3,11,10, 25, 30));
+
+        mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/consulta/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(consultaDTO)))
+                        .andDo(MockMvcResultHandlers.print())
+                        .andExpect(MockMvcResultMatchers.status().isOk())
+                        .andReturn();
+
+//        responseBody = mvcResult.getResponse().getContentAsString();
+        responseBody = mvcResult.getResponse().getContentAsString();
+
+        consultaDTO = objectFromString(ConsultaDTO.class, responseBody);
+
+        assertNotNull(consultaDTO.getId());
+//        assertNotEquals("Gabi", pacienteDTO.getNome());
+//        assertEquals("Gabriela", pacienteDTO.getNome());
+//        assertEquals("Carvalho", pacienteDTO.getSobreNome());
+//        assertEquals(1,pacienteDTO.getId());
+//        assertNotNull(enderecoDTO);
+//        assertEquals("Rua Rio Negro", enderecoDTO.getRua());
     }
 
     @Test
-    void getAll() throws Exception{
+    @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
+    void getById() throws Exception {
+    }
+
+    @Test
+    @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
+    void getAll() throws Exception {
     }
 
     @Test
@@ -190,21 +155,6 @@ class PacienteControllerTest {
 
     @Test
     @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
-    void getByName() throws Exception{
-    }
-
-    @Test
-    @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
-    void getBySurname() throws Exception{
-    }
-
-    @Test
-    @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
-    void getByRg() throws Exception{
-    }
-
-    @Test
-    @WithMockUser(username = "narayana", password = "123456789", roles = "ADMIN")
-    void getByDataDeAlta() throws Exception{
+    void getByPaciente() {
     }
 }
